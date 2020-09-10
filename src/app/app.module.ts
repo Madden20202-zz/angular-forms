703abaa57628eea0e290a93e222d6ecb2f00d11f
Austin <business.mocksock@gmail.com>
2020-09-09 19:23:30 -0700

    Added new files to showcase validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import {
 import { AppComponent } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+import { DemoFormWithValidationComponent } from './demo-form-with-validation/demo-form-with-validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
-    DemoFormSkuWithBuilderComponent
+    DemoFormSkuWithBuilderComponent,
+    DemoFormWithValidationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/demo-form-with-validation/demo-form-with-validation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-with-validation',
+  templateUrl: './demo-form-with-validation.component.html',
+  styleUrls: ['./demo-form-with-validation.component.css']
+})
+export class DemoFormWithValidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
