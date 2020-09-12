df97b3a5e6cc8d17d9b01d5040cd624e42d20440
Austin <business.mocksock@gmail.com>
2020-09-12 08:34:55 -0700

    Added validation logic and have the proper tag in

--- src/app/demo-form-with-validation/demo-form-with-validation.component.ts
+++ src/app/demo-form-with-validation/demo-form-with-validation.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  FormGroup, 
+  FormBuilder, 
+  Validators, 
+  AbstractControl 
+} from '@angular/forms';
 
 @Component({
   selector: 'app-demo-form-with-validation',
@@ -7,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoFormWithValidationComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  sku: AbstractControl;
+
+  constructor(fb: FormBuilder) { 
+    this.myForm = fb.group({
+      'sku': ['', Validators.required]
+    });
+
+    this.sku =  this.myForm.controls['sku'];
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(value: string): void{
+    console.log('You submitted value:', value)
   }
 
 }
